e259eed5b108cdb21235d7dec09c7909314b5059
Amit Gupta <amit73@gmail.com>
2026-08-04 10:16:11 +0530

    AWS Security group

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -12,3 +12,52 @@ provider "aws" {
   profile = "acme"
   region  = "us-east-1"
 }
+
+resource "aws_security_group" "web_sg" {
+
+  name        = "web-sg"
+
+  description = "Security group for Acme web server"
+
+  ingress {
+    description = "SSH"
+
+    from_port   = 22
+
+    to_port     = 22
+
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+
+    from_port   = 80
+
+    to_port     = 80
+
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port   = 0
+
+    to_port     = 0
+
+    protocol    = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+
+    Name = "Acme-Web-SG"
+
+  }
+
+}
