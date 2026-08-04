005be70c9c500b4003c6e1a052d864a1e81b3581
Amit Gupta <amit73@gmail.com>
2026-08-04 10:58:05 +0530

    default vpc

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -13,51 +13,41 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_security_group" "web_sg" {
-
   name        = "web-sg"
-
   description = "Security group for Acme web server"
 
+  vpc_id = data.aws_vpc.default.id
+  
   ingress {
     description = "SSH"
-
     from_port   = 22
-
     to_port     = 22
-
     protocol    = "tcp"
-
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "HTTP"
-
     from_port   = 80
-
     to_port     = 80
-
     protocol    = "tcp"
-
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-
     from_port   = 0
-
     to_port     = 0
-
     protocol    = "-1"
-
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-
     Name = "Acme-Web-SG"
-
   }
 
 }
